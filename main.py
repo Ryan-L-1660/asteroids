844efa29cc80abaf69f06e1ae5adf72efd98a9d2
ThePrimeagen <ryan.jamesnz07@gmail.com>
2025-03-15 13:31:59 +1300

    h

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
+# imports the pygame module
 import pygame
+from constants import *
+
+
+
+def main():
+    print("Starting Asteroids!")
+    print("Screen width:",SCREEN_WIDTH)
+    print("Screen height:",SCREEN_HEIGHT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
