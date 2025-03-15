df0f4510f4d23fcad06c8a658b8d33d455340cba
ThePrimeagen <ryan.jamesnz07@gmail.com>
2025-03-15 14:19:23 +1300

    turing

--- player.py
+++ player.py
@@ -8,9 +8,20 @@ class Player(CircleShape):
         self.rotation = 0
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
 
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_a]:
+            self.rotate(+dt)
+
+
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
 
     
     def triangle(self):
