14935823f262428bceed7154baa7417b0ba4057e
ThePrimeagen <ryan.jamesnz07@gmail.com>
2025-03-21 13:06:37 +1300

    changed difficulty

--- constants.py
+++ constants.py
@@ -7,6 +7,6 @@ PLAYER_SHOOT_COOLDOWN = 0.9 # <--- Cooldown on shooting
 SHOT_RADIUS = 2.5 # <--- How big each projectile is 
 ASTEROID_MIN_RADIUS = 22 # <--- smallest size of asteroid possible
 ASTEROID_KINDS = 3 # Small, Medium, Large
-ASTEROID_SPAWN_RATE = 5  # spawn rate
+ASTEROID_SPAWN_RATE = 1  # spawn rate
 ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS 
 PLAYER_RADIUS = 20 # <--- how big the player radius is 
--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def main(): # <---- main function declaration
 
 
     
-        
+        # Display font for score, weapon type, lives, and controls
         for sprite in drawable:
             sprite.draw(screen)
 
